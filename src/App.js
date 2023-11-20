28acad6c8d910f01f4cc9308cbac429ae7003b37
bibiayisha <bibiayisha15.2000@gmail.com>
2023-11-20 10:26:17 +0500

    slider ui

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import './App.css';
 import theme from './assets/themes/theme'; 
 import NavBar from './components/atoms/NavBar';
 import Header from './components/atoms/Header';
+import ImageCarousal from './components/molecules/ImageCarousal';
 
 function App() {
   return (
   <ThemeProvider theme={theme}>
     <NavBar />
     <Header/>
+    <ImageCarousal/>
   </ThemeProvider>
   );
 }
---
+++ src/components/utils/SlidesData.js
@@ -0,0 +1,25 @@
+// // App imports
+// import images from "./constants"
+
+// // slider 1
+// const slider1 = [
+//     {
+//         heading: '',
+//         subtext:  '',
+//         image:images.header.logo,
+//         videoLink: ''
+//     },
+//     {
+//         heading: '',
+//         subtext:  '',
+//         image:images.header.logo,
+//         videoLink: ''
+//     },
+//     {
+//         heading: '',
+//         subtext:  '',
+//         image:images.header.logo,
+//         videoLink: ''
+//     },
+    
+// ]
