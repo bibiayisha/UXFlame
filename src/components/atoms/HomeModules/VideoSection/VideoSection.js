9aa41fb9fd67cf0b367bf74a9fff491b61e1733a
bibiayisha <bibiayisha15.2000@gmail.com>
2024-02-16 23:18:52 +0500

    video-sec set for tab

--- src/components/atoms/HomeModules/VideoSection/VideoSection.js
+++ src/components/atoms/HomeModules/VideoSection/VideoSection.js
@@ -10,10 +10,10 @@ import VideoThumbnail from '../../VideoThumbnail/VideoThumbnail'
 function VideoSection() {
   return (
     <div className='bg-darkBlue py-20'>
-        <div className='video-sec relative' >
-        <Grid container alignItems="stretch" className='g relative container pb-10 md:pb-0 bg-orange md:bg-transparent'>
+        <div className='video-sec relative bg-orange lg:bg-transparent' >
+        <Grid container alignItems="stretch" className='g relative container pb-10 lg:pb-0 bg-orange lg:bg-transparent'>
         <Grid item xs={12} md={6} lg={7} >
-            <div className='flex flex-col h-full justify-center  md:py-0 py-10 text-center md:text-left  bg-orange'>
+            <div className='flex flex-col h-full justify-center  lg:py-0 py-10 text-center lg:text-left  bg-orange'>
             <div>
                 <p className='text-white text-fs-16 font-light uppercase'>unbox spotlight</p>
             </div>
