7c9a73538cacec533ee014258e80799da1ed8335
bibiayisha <bibiayisha15.2000@gmail.com>
2024-04-17 22:04:45 +0500

    resolve issues

--- src/components/atoms/AboutUsModules/StatsCard/StatsCard1.js
+++ src/components/atoms/AboutUsModules/StatsCard/StatsCard1.js
@@ -10,9 +10,9 @@ const StatsCard1 = ({stats_n, txt1, txt2}) =>{
     <div className='bg-white relative flex flex-col justify-center items-center'>
         <div className='relative w-full text-center'><img width={'100%'} src={images.body.stats_img1}/></div>
         <div className='absolute transform -translate-x-[0] -translate-y-[20%]'>
-        <Typography variant='h3' className='text-white pb-1 capitalize text-center '>{stats_n}</Typography>
-        <p className='text-white text-fs-18  tracking-ls-neg4 text-center '>{txt1}</p>
-        <p className='text-gray text-fs-14  tracking-ls-neg4 text-center '>{txt2}</p>
+        <p className='text-white pb-1 capitalize text-center text-3xl md:text-5xl font-bold'>{stats_n}</p>
+        <p className='text-white text-sm md:text-base   tracking-ls-neg4 text-center '>{txt1}</p>
+        <p className='text-gray md:text-sm text-xs  tracking-ls-neg4 text-center '>{txt2}</p>
         </div>
     </div>
   )
